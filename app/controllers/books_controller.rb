6393e3af142759016ee4c0923d6e0d3bd65d052a
shumin0809 <shumin0809@yahoo.com>
2014-07-17 20:44:37 -0700

    finish part of post my stuff

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_filter :check_user, :only=>[:index, :update]
   def index
     @books = Book.where(params[:bookSearchType].to_sym => params[:search])
     if @books.empty?
@@ -11,5 +12,38 @@ class BooksController < ApplicationController
     @book ||= Book.find(params[:id])
   end
 
+  def new
+    @book = Book.new 
+  end
+
+  def create
+    @book = processISBN
+    if @book.nil?
+      flash[:alert] = "Invalid ISBN!"
+      redirect_to new_book_path
+    else
+      redirect_to new_bookprice_path
+    end
+  end
+
+  private
+    def processISBN
+      book = Book.find_by(:isbn => params[:book][:isbn]) || fetch_book(params[:book][:isbn])
+    end
+
+    def fetch_book(isbn)
+      url = "https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}"
+      result = JSON.load(open(url).read)
+      if result["totalItems"] == 0
+        nil
+      else
+        book = Book.create(
+          :title => result["items"][0]["volumeInfo"]["title"],
+          :isbn => isbn,
+          :author => result["items"][0]["volumeInfo"]["authors"][0] || " ",
+          :pic_url => result["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+        )
+      end
+    end
 
 end
