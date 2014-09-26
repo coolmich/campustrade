80014affb47fee78c9360319af245a04177386a7
bovey <boveylisi@gmail.com>
2014-09-26 10:30:26 +0800

    improve car page UI

--- app/assets/javascripts/car.js
+++ app/assets/javascripts/car.js
@@ -1,5 +1,15 @@
 $(function(){
   $('.car-card').on('click', function(){
-    $(this).children('.car-detail').slideToggle();
+    if (!(event.target.className === "img-small")){
+      var txt = $(".car-detail").is(':visible') ? 'Click to view detail' : 'Click to hide detail';
+      $(this).children('.car-detail').slideToggle();
+      $(this).find('.car-detail-button').text(txt);
+      $(this).find('.car-detail-button').toggleClass('car-detail-button-active');
+    }
+  });
+
+  $('.img-small').on('click', function() {
+    var image_src = $(this).attr('src');
+    $(this).parent().siblings('.img-big').attr('src', image_src);
   });
 });
