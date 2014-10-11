8b3aca9ad4c2999a7a3f2cdc0c36569c792eaf7b
Nan Chen <itisnanful@gmail.com>
2014-10-11 15:47:45 -0700

    guojiakai and lixueyang added to about page

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -2,16 +2,25 @@ $(function(){
   $('.role-master').click(function() {
     $('.role-marketing-container')[0].style.display = "none";
     $('.role-technical-container')[0].style.display = "none";
+    $('.role-intern-container')[0].style.display = "none";
     $('.role-master-container').fadeIn();
   });
   $('.role-marketing').click(function() {
     $('.role-master-container')[0].style.display = "none";
     $('.role-technical-container')[0].style.display = "none";
+    $('.role-intern-container')[0].style.display = "none";
     $('.role-marketing-container').fadeIn();
+  });
+  $('.role-intern').click(function() {
+    $('.role-master-container')[0].style.display = "none";
+    $('.role-technical-container')[0].style.display = "none";
+    $('.role-marketing-container')[0].style.display = "none";
+    $('.role-intern-container').fadeIn();
   });
   $('.role-technical').click(function() {
     $('.role-master-container')[0].style.display = "none";
     $('.role-marketing-container')[0].style.display = "none";
+    $('.role-intern-container')[0].style.display = "none";
     $('.role-technical-container').fadeIn();
   });
 });
